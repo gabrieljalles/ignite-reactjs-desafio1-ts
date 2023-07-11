6caf3b2886a69be7cf1be4f606984c0295e36093
Gabriel Jalles Ferreira Mota <gabriel.jalleswork@hotmail.com>
2023-07-11 16:57:21 -0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 
 import styles from './App.module.css'
 import { Header } from './components/Header'
+import Task from './components/Task'
 
 function App() {
+
+  const tasks: taskType[] = [
+    {
+      id:1,
+    }
+  ]
 
   return (
       <div className={styles.layout}>
         <Header />
-        <div></div>
+
+        <main>
+            {tasks.map(post=> {
+              return(
+                <Task 
+                  key={post.id}
+                  post={post}
+                />
+              )
+            })}
+        </main>
       </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
-import styles from '.Header.module.css';
+import styles from './Header.module.css';
 import toDoLogo from '../assets/todo-logo.svg';
 
 export const Header = () =>{
     return(
-        <header className={styles.header}>
+        <header >
+            <div className={styles.header}>
             <img src={toDoLogo} alt="Logotipo do ToDo"/>
+            </div>
+            <div>
+                <NewTask />
+            </div>
         </header>
     )
 }
